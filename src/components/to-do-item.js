e373f086b840237659bdbc94d38855bb8116a09c
whatapalaver <warriorwomenblog@gmail.com>
2019-04-02 14:41:33 +0100

    Part 3 using lit-element

--- src/components/to-do-item.js
+++ src/components/to-do-item.js
@@ -1,90 +1,62 @@
-import { html, render } from 'lit-html';
+import { LitElement, html, css } from 'lit-element';
 
-class TodoItem extends HTMLElement {
+class TodoItem extends LitElement {
+    static get properties() {
+        return {
+            text: { 
+                type: String,
+                reflect: true
+            },
+            checked: { 
+                type: Boolean, 
+                reflect: true 
+            },
+            index: { type: Number }
+        }
+    }
+
     constructor() {
         super();
-        this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+        // set some default values
+        this.text = '';
+        this.checked = false;
     }
 
-    connectedCallback() {
-        if(!this.hasAttribute('text')) {
-            this.setAttribute('text', 'placeholder');
-        }
-        render(this.template(), this._shadowRoot, {eventContext: this});
+    _fire(eventType) {
+        this.dispatchEvent(new CustomEvent(eventType, { detail: this.index }));   
     }
 
-    _fireToggle(e) {
-        this.dispatchEvent(new CustomEvent('onToggle', { detail: this.index }));
-    }
-    _fireRemove(e) {
-        this.dispatchEvent(new CustomEvent('onRemove', { detail: this.index }));
+    static get styles() {
+      return css`
+      :host {
+        display: block;
+        font-family: sans-serif;
+      }
+
+      .completed {
+        text-decoration: line-through;
+      }
+
+      button {
+        cursor: pointer;
+        border: none;
+      }
+      `;
     }
 
-    template() {
+    render() {
         return html`
-            <style>
-                :host {
-                    display: block;
-                    font-family: sans-serif;
-                }
-                .completed {
-                    text-decoration: line-through;
-                }
-                button {
-                    border: none;
-                    cursor: pointer;
-                    background-color: Transparent;
-                }
-            </style>
             <li class="item">
                 <input 
                     type="checkbox" 
-                    .checked=${this._checked}
-                    @change=${this._fireToggle}>
-                <label class=${this._checked ? 'completed' : ''}>${this._text}</label>
-                <button @click=${this._fireRemove}>❌</button>
+                    ?checked=${this.checked} 
+                    @change=${() => this._fire('onToggle')}>
+                </input>
+                <label class=${this.checked ? 'completed' : ''}>${this.text}</label>
+                <button @click=${() => this._fire('onRemove')}>❌</button>
             </li>
         `;
     }
-
-    static get observedAttributes() {
-        return ['text', 'checked', 'index'];
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-        switch(name){
-            case 'text':
-                this._text = newValue;
-                break;
-            case 'checked':
-                this._checked = this.hasAttribute('checked');
-                break;
-            case 'index':
-                this._index = parseInt(newValue);
-                break;
-        }
-        render(this.template(), this._shadowRoot, {eventContext: this});
-    }
-
-    set index(val) {
-        this.setAttribute('index', val);
-    }
-
-    get index() {
-        return this._index;
-    }
-
-    get checked() {
-        return this.hasAttribute('checked');
-    }
-
-    set checked(val) {
-        if (val) {
-            this.setAttribute('checked', '');
-        } else {
-            this.removeAttribute('checked');
-        }
-    }
-
 }
+
 window.customElements.define('to-do-item', TodoItem);
